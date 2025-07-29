a10922d5a486a6fe35d98b15a7bdc787c2ffc54e
JJavierMartinez <jamartinez8595@gmail.com>
2025-07-29 17:14:17 +0200

    Iniciando proyecto y repositorio

---
+++ src/main/java/es/cic/curso25/proy08/Proy08Application.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.proy08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proy08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proy08Application.class, args);
+	}
+
+}
